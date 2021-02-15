aaa35c2efdedf7aa037371429d56708ce9884a8d
leandroprada <leandroprada@gmail.com>
2021-02-15 12:40:28 -0300

    added curso basico y avanzado feb 21

--- content/resources/resources_itbasicofeb21.php
+++ content/resources/resources_itbasicofeb21.php
@@ -10,25 +10,25 @@
 		  <div class="col-md-12 col-sm-12">
 			  <div class="video-promo-content text-center">
 				<ul>
-					<li style="text-decoration:none;padding-bottom:10px; "><h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT M&oacute;dulo 1 Clase 1 12-Feb-2021 <a  href="#submenubasicoene211" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-lg"  style="color:white;">Ver</a></h2>							
-						<div class="collapse list-unstyled" id="submenubasicoene211">
+					<li style="text-decoration:none;padding-bottom:10px; "><h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT M&oacute;dulo 1 Clase 1 12-Feb-2021 <a  href="#submenubasicofeb211" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-lg"  style="color:white;">Ver</a></h2>							
+						<div class="collapse list-unstyled" id="submenubasicofeb211">
 							
 							<iframe width="560" height="315" src="https://youtu.be/No4Ejx1yuXU" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 						</div>
 					</li>
-					<li style="text-decoration:none;padding-bottom:10px; "> <h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT M&oacute;dulo 1 Clase 2 18-Feb-2021 <a  href="#submenubasicoene212" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-lg"  style="color:white;">Ver </a></h2>							
-						<div class="collapse list-unstyled" id="submenubasicoene212">
+					<li style="text-decoration:none;padding-bottom:10px; "> <h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT M&oacute;dulo 1 Clase 2 18-Feb-2021 <a  href="#submenubasicofeb212" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-lg"  style="color:white;">Ver </a></h2>							
+						<div class="collapse list-unstyled" id="submenubasicofeb212">
 							<iframe width="560" height="315" src="https://www.youtube.com/embed/gNay8n9Xhnc" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 						</div>
 					</li>
-					<li style="text-decoration:none;padding-bottom:10px; "><h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT M&oacute;dulo 1 Clase 3 26-Feb-2021 <a  href="#submenubasicoene213" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-lg"  style="color:white;">Ver </a> </h2>
-						<div class="collapse list-unstyled" id="submenubasicoene213">
+					<li style="text-decoration:none;padding-bottom:10px; "><h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT M&oacute;dulo 1 Clase 3 26-Feb-2021 <a  href="#submenubasicofeb213" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-lg"  style="color:white;">Ver </a> </h2>
+						<div class="collapse list-unstyled" id="submenubasicofeb213">
 													
 							<iframe width="560" height="315" src="https://www.youtube.com/embed/gNay8n9Xhnc" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 						</div>
 					</li>
-					<!-- <li style="text-decoration:none;padding-bottom:10px; "><h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT M&oacute;dulo 2 Bonus Clase 4 12-SEP-2020 <a  href="#submenubasicoene214" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-lg"  style="color:white;">Ver </a> </h2>
-						<div class="collapse list-unstyled" id="submenubasicoene214">
+					<!-- <li style="text-decoration:none;padding-bottom:10px; "><h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT M&oacute;dulo 2 Bonus Clase 4 12-SEP-2020 <a  href="#submenubasicofeb214" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-lg"  style="color:white;">Ver </a> </h2>
+						<div class="collapse list-unstyled" id="submenubasicofeb214">
 													
 							<video width="75%" height="450px"  controls>
 							  <source src="content/resources/videos/itavanzadonov20-4.mp4" type="video/mp4">
